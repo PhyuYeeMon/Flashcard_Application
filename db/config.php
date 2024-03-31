75125e17544a89d64a821ac36b413b32d6201ef5
phyuyeemon <phyuyeemon18@gmail.com>
2024-03-31 21:08:04 +0630

    Modify db password in config.php

--- db/config.php
+++ db/config.php
@@ -1,7 +1,7 @@
 <?php
 	$host="localhost";
 	$username="root";
-	$password="phyunu";
+	$password="root";
 	$database="flashcard";
 	try{
 		$connection=new PDO("mysql:host=$host;dbname=$database","$username","$password");
